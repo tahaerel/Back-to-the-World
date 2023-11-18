adda3e864ce1ba3dc21d57d1f4f663da9460ba0a
Taha Erel <tahaerelll@gmail.com>
2023-11-19 01:29:40 +0300

    updates

--- Assets/Script/onizlemeobje.cs
+++ Assets/Script/onizlemeobje.cs
@@ -5,25 +5,31 @@ using UnityEngine;
 public class onizlemeobje : MonoBehaviour
 {
     public GameObject Olusacakobje;
+    public float gridSize = 1.0f; // Kare boyutu
     RaycastHit hit;
     public Material materyal;
     bool olusturabilirmi;
     public MeshRenderer matcolor;
 
-
     void Start()
     {
         olusturabilirmi = true;
+        UpdatePosition();
+    }
+
+    void UpdatePosition()
+    {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(ray,out hit,5000f,(1<<8)))
+        if (Physics.Raycast(ray, out hit, 5000f, (1 << 8)))
         {
+            Vector3 gridPosition = hit.point;
+            gridPosition.x = Mathf.Floor(gridPosition.x / gridSize) * gridSize + gridSize / 2;
+            gridPosition.y = Mathf.Floor(gridPosition.y / gridSize) * gridSize + gridSize / 2;
+            gridPosition.z = Mathf.Floor(gridPosition.z / gridSize) * gridSize + gridSize / 2;
 
-            //  transform.position = hit.point;
-            transform.position = new Vector3(hit.point.x, hit.point.y + .5f, hit.point.z);
+            transform.position = gridPosition;
         }
-       
-       
     }
 
     private void OnTriggerStay(Collider other)
@@ -34,17 +40,13 @@ public class onizlemeobje : MonoBehaviour
 
             GetComponent<MeshRenderer>().material.color = Color.red;
 
-            for (int i = 0; i <matcolor.materials.Length; i++)
+            for (int i = 0; i < matcolor.materials.Length; i++)
             {
-                
-            matcolor.materials[i].color= Color.red;
+                matcolor.materials[i].color = Color.red;
             }
             olusturabilirmi = false;
-
         }
-      
     }
-
 
     private void OnTriggerExit(Collider other)
     {
@@ -54,33 +56,18 @@ public class onizlemeobje : MonoBehaviour
 
             for (int i = 0; i < matcolor.materials.Length; i++)
             {
-
                 matcolor.materials[i].CopyPropertiesFromMaterial(materyal);
-
             }
             olusturabilirmi = true;
-
         }
     }
 
-   
-
     void Update()
     {
-
-
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (Physics.Raycast(ray, out hit, 5000f, (1 << 8)))
-        {
-
-            transform.position = new Vector3(hit.point.x, hit.point.y + .5f, hit.point.z);
-
-        }
+        UpdatePosition();
 
         if (Input.GetMouseButton(0))
         {
-
             if (olusturabilirmi)
             {
                 Instantiate(Olusacakobje, transform.position, transform.rotation);
@@ -90,11 +77,7 @@ public class onizlemeobje : MonoBehaviour
 
         if (Input.GetMouseButton(1))
         {
-
-                Destroy(gameObject);
-          
+            Destroy(gameObject);
         }
-
-
     }
 }
