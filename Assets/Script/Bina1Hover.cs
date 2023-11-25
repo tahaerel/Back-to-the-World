26b02aaa569e67db4cb8b93fc8b2e3c62a9704ea
Taha Erel <tahaerelll@gmail.com>
2023-11-25 17:14:12 +0300

    latest

--- Assets/Script/Bina1Hover.cs
+++ Assets/Script/Bina1Hover.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEditor.ShaderGraph.Serialization;
 
 public class Bina1Hover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
